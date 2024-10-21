a634e980df374b69318b1b0a3231b48ab0467e08
vortex2jm <joaopaulomoura5328@gmail.com>
2024-10-21 18:12:55 -0300

    :construction: Continue the code development

--- main.py
+++ main.py
@@ -2,82 +2,126 @@ from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 import requests
 import os
+import utils
+import csv
 
-load_dotenv()
+def login(login_url):
+    session = requests.Session()            # Setting up session
+    login_page_res = session.get(login_url)     # Getting login page
 
-login_url = os.getenv('LOGIN_URL')
-data_url = os.getenv('DATA_URL')
+    if login_page_res.status_code == 200:
+        login_page_html = BeautifulSoup(login_page_res.text, 'html.parser')
+        csrf_token = login_page_html.find('input', {'name': 'csrfmiddlewaretoken'})['value']       # Cross Site Request Forgery token
+        payload = {
+            'username': os.getenv('USER'),
+            'password': os.getenv('PASSWORD'),
+            'csrfmiddlewaretoken': csrf_token,
+            'next': '/admin'
+        }   # Auth data
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            'Referer': login_url        # Extremely necessary
+        }   # Req header
 
-# Setting up session
-session = requests.Session()
-
-# Getting login page
-login_page = session.get(login_url)
-
-if login_page.status_code == 200:
-
-    login_page_html = BeautifulSoup(login_page.text, 'html.parser')
-    
-    # Cross Site Request Forgery token
-    csrf_token = login_page_html.find('input', {'name': 'csrfmiddlewaretoken'})['value']
-
-    # Authentication data
-    payload = {
-        'username': os.getenv('USER'),
-        'password': os.getenv('PASSWORD'),
-        'csrfmiddlewaretoken': csrf_token,
-        'next': '/admin'
-    }
-
-    # Request header
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        'Referer': login_url        # Extremely necessary
-    }
-
-    # Doing login
-    response = session.post(login_url, data=payload, headers=headers)
-    if response.status_code == 200:
-        print("Login succesfully!")
-    else:
-        print(f"Could not login: {response.status_code}")
-        exit(1)
-
-    # for i in range(100):
-        # url = f"{os.getenv('DATA_URL')}{i}/change/"
-        # res =session.head(url) 
-        # if res.status_code == 200:
-            # print(i, res.url)
-            
-
-    url = f"{os.getenv('DATA_URL')}{37}/change/"
-    res = session.get(url)
-    client_data = BeautifulSoup(res.text, 'html.parser')
-    
-    labels = client_data.find_all('label')
-    inputs = client_data.find_all('input')
-
-    for label, input_field in zip(labels, inputs):
-        label_text = label.get_text(strip=True)
-        # input_value = input_field['value']
-        input_value = input_field.get('value', 'Valor não disponível') 
-        print(f"Texto do Label: {label_text}, Valor do Input: {input_value}")
-
-    # print(client_data)
+        login_res = session.post(login_url, data=payload, headers=headers)   # Doing login
+        
+        if login_res.status_code == 200:
+            print("Login succesfully!")
+            return session
+        else:
+            print(f"Could not login: {login_res.status_code}")
+            exit(1)
 
 
-    # print(page_id)
+if __name__ == "__main__":
+    load_dotenv()
 
-    # Accessing clients pages
-    # data_response = session.get(data_url)
+    session = login(os.getenv('LOGIN_URL'))
+    is_first_iter = True
 
-    # Analisar o conteúdo
-    # client_data = BeautifulSoup(data_response.text, 'html.parser')
+    for x in range(100):
 
-    # Extrair os dados desejados
-    # Por exemplo, extrair todos os títulos de uma lista
-    # for title in client_data.find_all('h1'):
-        # print(title.get_text())
+        url = f"{os.getenv('DATA_URL')}{x}/change/"
+
+        res = session.get(url)
+        print(res.url)
+        print(url)
+
+        if res.url != url:
+            continue    
+
+        client_data = BeautifulSoup(res.text, 'html.parser')
+
+        # labels = client_data.find_all('label')
+        # inputs = client_data.find_all('input')
+        # inputs.pop(0)
+
+        general_info = {}
+
+        for inp_id in utils.INPT_IDS:
+            input_element = client_data.find('input', id=inp_id)
+            if input_element:
+                general_info[inp_id.replace('id_','')] = input_element.get('value', '-')
 
 
-# if __name__ == "__main__":
+        for slct_id in utils.SLCT_IDS:
+            select_element = client_data.find('select', id=slct_id)
+            if select_element:
+                selected_option = select_element.find('option', selected=True)
+                general_info[slct_id.replace('id_','')] = selected_option.text
+
+        # print(general_info)
+
+
+        phones = {}
+
+        i = 0
+        while True:
+            phone_id = f'{utils.PARTIAL_PHONE_ID}{i}'
+            tr_element = client_data.find('tr', id=phone_id)
+            if not tr_element:
+                break
+
+            phone = {}
+            for td_class in utils.TD_CLASSES:
+                td_element = tr_element.find('td', class_=td_class)
+                input_element = td_element.find('input')
+                if input_element:
+                    phone[td_class.replace('field-','')] = input_element.get('value', '-')
+                    continue
+                select_element = td_element.find('select')
+                if select_element:
+                    selected_option = select_element.find('option', selected=True)
+                    phone[td_class.replace('field-','')] = selected_option.text
+            phones[phone_id.replace('_set-','')] = phone
+            i+=1
+
+        # print(phones)
+
+
+        address = {}
+        for addr_id in utils.ADDR_IDS:
+            input_element = client_data.find('input', id=addr_id)
+            if input_element:
+                address[addr_id.replace('id_endereco-0-','')] = input_element.get('value', '-')
+        # print(address)
+
+
+        operator = {}
+        select_element = client_data.find('select', id=utils.OPERATOR_ID)
+        if select_element:
+            selected_option = select_element.find('option', selected=True)
+            operator["operadora"] = selected_option.text
+        # print(operator)
+
+
+        data = {**general_info, **phones, **address, **operator}
+        # print(data)
+
+        file_name = 'data.csv'
+        with open(file_name, mode='a') as csv_file:
+            writer = csv.writer(csv_file)
+            if is_first_iter:
+                writer.writerow(data.keys())
+                is_first_iter = False
+            writer.writerow(data.values())
---
+++ utils.py
@@ -0,0 +1,41 @@
+INPT_IDS = [
+    'id_nome',
+    'id_email',
+    'id_cnpj',
+    'id_descricao',
+    'id_horario_acesso',
+    'id_horario_acesso_fim'
+]
+
+SLCT_IDS = [
+    'id_responsavel_tecnico',
+    'id_responsavel_administrativo',
+    'id_responsavel_seguranca',
+    'id_ativo_rede_cliente',
+    'id_valida_conf'
+]
+
+PARTIAL_PHONE_ID = 'telefone_set-'
+
+TD_CLASSES = [
+    'field-ddd',
+    'field-numero',
+    'field-ramal',
+    'field-pessoa',
+    'field-obs'
+]
+
+ADDR_IDS = [
+    'id_endereco-0-logradouro',
+    'id_endereco-0-complemento',
+    'id_endereco-0-numero',
+    'id_endereco-0-bairro',
+    'id_endereco-0-cidade',
+    'id_endereco-0-estado',
+    'id_endereco-0-pais',
+    'id_endereco-0-cep'
+]
+
+OPERATOR_ID = 'id_clientexoperadora_set-0-operadora'
+
+# Other info in div class=readonly in div class=resp...Seg,Tec,Adm
